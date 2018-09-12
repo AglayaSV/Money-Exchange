ec447571dfe3a07287714da9fdf453e7fad5c37a
aglaya <aglayastunkus1@gmail.com>
2018-09-12 15:09:57 +0300

    style: delete empty strings

--- src/index.js
+++ src/index.js
@@ -12,32 +12,22 @@ module.exports = function makeExchange(currency) {
     }
     if (currency >= 50) {
         exchangeObg.H = (currency - currency % 50) / 50;//how many 50
-
     }
     let remain50 = currency % 50;
     if (remain50 >= 25) {
         exchangeObg.Q = (remain50 - remain50 % 25) / 25;// how many 25
     }
     let remain25 = remain50 % 25;
-
     if (remain25 >= 10) {
         exchangeObg.D = (remain25 - remain25 % 10) / 10;// how many 10
-
-
     }
     let remain10 = remain25 % 10;
     if (remain10 >= 5) {
         exchangeObg.N = (remain10 - remain10 % 5) / 5;// how many 5
     }
-
     let remain5 = remain10 % 5;
-
     if (remain5 >= 1) {
-        exchangeObg.P = remain5 ;// how many 1
-
+        exchangeObg.P = remain5;// how many 1
     }
-
     return exchangeObg;
-
-
-}
+};
